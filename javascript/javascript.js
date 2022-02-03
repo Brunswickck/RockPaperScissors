fcdca8be6effdd3f685da7656051afd5aa1728b4
Tom Burns <tomburns.gj@gmail.com>
2022-02-03 04:38:20 +0100

    cleaned up the .js file to include most of the variables into the play round function.
    This allows multiple prompts to display and
    therefore multiple rounds to be played.
    
    Added a simple end game function.

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,48 +1,63 @@
+//Initial scores & round//
+
+let playerScore = 0;
+let computerScore = 0;
+let roundNumber = 0;
 
 //code to generate computers choice//
 
-const computerNumber = Math.floor(Math.random()*3);
-
-const computerOptions = ['rock','paper','scissors'];
-
-const computerChoice = computerOptions[computerNumber];
-
-
-//code for players choice//
-
-let playerChoiceCaseSensitive = prompt("rock, paper or scissors?");
-
-let playerChoice = playerChoiceCaseSensitive.toLowerCase();
-
-/* if (playerChoice != "rock" || playerChoice != "paper" || playerChoice != "scissors") {
-   alert("You have not selected 'rock', 'paper' or 'scissors'."); 
-} */
-
+function computerPlay() {
+    let computerNumber = Math.floor(Math.random()*3);
+    let computerOptions = ['rock','paper','scissors'];
+    return computerOptions[computerNumber];
+}
 
 //code for playing a round//
 
-function playRound(playerChoice, computerChoice) {
-    if (playerChoice === computerChoice) {
-        alert(`You both picked ${playerChoice}, it's a draw!`)
-    }
-    else if (
-        (playerChoice === 'rock' && computerChoice === 'scissors') || 
-        (playerChoice === 'scissors' && computerChoice === 'paper') || 
-        (playerChoice === 'paper' && computerChoice === 'rock')) {
-            alert(`You win! ${playerChoice} beats ${computerChoice}.`)
-        }
+function playRound() {
 
-    else if (
-        (computerChoice === 'rock' && playerChoice === 'scissors') || 
-        (computerChoice === 'scissors' && playerChoice === 'paper') || 
-        (computerChoice === 'paper' && playerChoice === 'rock')) {
-            alert(`You lose! ${computerChoice} beats ${playerChoice}.`)
+    let playerChoice = prompt("rock, paper or scissors?").toLowerCase(); // code for player choice
+    let computerChoice = computerPlay();
+
+        if (playerChoice === computerChoice) {
+            alert(`You both picked ${playerChoice}, it's a draw!`);
+            alert(`Your score is ${playerScore} and the computers score is ${computerScore}.`);
+            roundNumber++;
+            
         }
+        else if (
+            (playerChoice === 'rock' && computerChoice === 'scissors') || 
+            (playerChoice === 'scissors' && computerChoice === 'paper') || 
+            (playerChoice === 'paper' && computerChoice === 'rock')) {
+                playerScore++;
+                roundNumber++;
+                alert(`You win! ${playerChoice} beats ${computerChoice}.`);
+                alert(`Your score is ${playerScore} and the computers score is ${computerScore}.`);
+            }
+
+        else if (
+            (computerChoice === 'rock' && playerChoice === 'scissors') || 
+            (computerChoice === 'scissors' && playerChoice === 'paper') || 
+            (computerChoice === 'paper' && playerChoice === 'rock')) {
+                computerScore++;
+                roundNumber++;
+                alert(`You lose! ${computerChoice} beats ${playerChoice}.`);
+                alert(`Your score is ${playerScore} and the computers score is ${computerScore}.`);
+                
+            }
+        else {
+            alert("error.")
+        }
+};
+
+function endgame() {
+    if (playerScore > computerScore) {
+        alert("You reached 5 points first. You win!")
+    }
+    else if (computerScore > playerScore) {
+        alert("The computer reached 5 points first. You lose...")
+    }
     else {
         alert("error.")
     }
-};
-
-/* const playerChoice = "rock"; */
-
-console.log(playRound(playerChoice, computerChoice))
+}
